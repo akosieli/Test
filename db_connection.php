c6f601ac5b8269bb7e1161076645c2f0a270baa4
Eli Ken <eliken.ramos27@gmail.com>
2025-10-14 02:19:53 +0800

    created DB connection

---
+++ db_connection.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";     
+    $db_name = "user_db";
+
+    $conn = new mysqli($host, $user, $password, $db_name);
+
+    if ($conn->connect_error){
+        die("Connection Failed: " . $conn->connect_error);
+    }
+?>
+
